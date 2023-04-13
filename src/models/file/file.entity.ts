b7547f57ae0e5eeabad852e3961bd91a7e4bbfd7
Ihor_S <ihor.shcherbyna@clockwise.software>
2023-04-13 11:08:39 +0200

    remove mestjs/graphql decorators because they need a lot of nestJs libs

--- src/models/file/file.entity.ts
+++ src/models/file/file.entity.ts
@@ -7,29 +7,22 @@ import {
 } from 'typeorm';
 import { TableNameConst } from '../../constants/table-name.constant';
 import type { RelationshipPostFile } from '../discussion/relationship-post-file.entity';
-import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @Entity({ name: TableNameConst.FILES })
-@ObjectType()
 export class File {
   @PrimaryGeneratedColumn('increment', { type: 'integer' })
-  @Field(() => Int)
   id!: number;
 
   @Column('varchar', { name: 'file_name' })
-  @Field(() => String)
   fileName!: string;
 
   @Column('bigint', { name: 'file_size' })
-  @Field(() => Int)
   fileSize!: number;
 
   @Column('varchar', { name: 'file_type' })
-  @Field(() => String)
   fileType!: string;
 
   @Column('varchar', { name: 'file_url' })
-  @Field(() => String)
   fileUrl!: string;
 
   @OneToOne(
